68aa7bb3315553bf8537d1308d1304f2a316ff81
Maksim Kormiltsev <newarty28@gmail.com>
2021-07-31 17:12:52 +0300

    first version

---
+++ js/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+function handleSubmit(evt) {
+    evt.preventDefault();
+
+    const phone = phoneInputEl.value.trim();
+
+    const data = {
+        phone,
+    };
+
+    //очищаем поля
+    messageEl.textContent = '';
+    phoneErrorEl.textContent = '';
+    phoneInputEl.textContent = '';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:9999/api/hw14');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = handleSuccess;
+    xhr.onerror = handleError;
+    xhr.send(JSON.stringify(data));
+}
+
+function handleSuccess(evt) {
+    if (evt.target.status !== 200) {
+        phoneErrorEl.textContent = data.error;
+        messageEl.textContent = 'Произошла ошибка';
+        return;
+    }
+
+    const data = JSON.parse(evt.target.responseText);
+    messageEl.textContent = 'Успешно отправлено! Наш менеджер перезвонит в течение 15 минут.';
+}
+
+function handleError(evt) {
+    if (evt.target.status === 400) {
+        messageEl.textContent = 'Произошла ошибка';
+        const data = JSON.parse(evt.target.responseText);
+        phoneErrorEl.textContent = data.error;
+        return;
+    }
+}
+
+const formEl = document.getElementById('callback-form');
+formEl.addEventListener('submit',handleSubmit);
+
+const phoneInputEl = document.getElementById('phone-input');
+const phoneErrorEl = document.getElementById('phone-error');
+const messageEl = document.getElementById('message');
